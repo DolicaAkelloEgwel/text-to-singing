597062a17cf4e8645ddb4b3977f08457de2c1894
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2025-02-11 15:41:27 +0000

    wav conversion

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 import speech_recognition as sr
 from pydub import AudioSegment, silence
 import os
-import shutil
 
 # use cwd to set root dir path
-ROOT_DIR = os.path.join(os.getcwd(), "files")
+FILES_DIR = os.path.join(os.getcwd(), "files")
 
 # change this
 name_of_recording = "training_voice_15min.mp3"
 
 # other folders
-rec_src_dir = f"{ROOT_DIR}/{name_of_recording}"
-input_file = f"{ROOT_DIR}/wavs/{name_of_recording}"
+rec_src_dir = os.path.join(FILES_DIR, name_of_recording)
 
+# get the file extension
+file_ext = os.path.splitext(rec_src_dir)[1]
+
+if file_ext == ".wav":
+    # the file is already in WAV format
+    audio = AudioSegment.from_wav(rec_src_dir)
+else:
+    # the file is in MP3 format, so we need to convert it to WAV
+    audio = AudioSegment.from_file(rec_src_dir)
+    output_file = os.path.splitext(rec_src_dir)[0] + ".wav"
+    audio.export(output_file, format="wav")
+    audio = AudioSegment.from_wav(output_file)
+
+desired_sr = 22050
